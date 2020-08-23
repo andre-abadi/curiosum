65aebff2050029279c35de43839fd73284752b06
dancingborg <dancingborg@users.noreply.github.com>
2020-08-23 21:32:50 +0800

    tidy up

--- curiosum.sh
+++ curiosum.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+
 # change these variables as required
 address="rtsp://10.1.2.63:554/s1"
 data="/volume1/transfer/curiosum"
+
 
 # you shoudln't need to change these variables
 today=$(date +"%Y-%m-%d-%H-%M-%S-%Z")
@@ -12,6 +14,7 @@ line="======================================================"
 # below  is because otherwise a 1s file gets made after duration
 let split=duration+1
 
+
 # preamble
 echo $line >> $logfile
 printf "Curiosum audio-only RTSP recorder.\n" | tee -a $logfile
@@ -20,8 +23,9 @@ printf "  Storing data in: '%s'\n" "${data}" | tee -a $logfile
 printf "  Checking that subdirectories /recordings and /logs exist.\n" | tee -a $logfile
 mkdir -p "${data}" "${data}/logs" "${data}/recordings" | tee -a $logfile
 printf "  Logging to: '%s'\n" "${logfile}" | tee -a $logfile
-printf "Running openRTSP for: '%s's\n" "${duration}" | tee -a $logfile
+printf "  Running openRTSP for: '%s's\n" "${duration}" | tee -a $logfile
 echo $line >> $logfile
+
 
 # execution
 openRTSP \
@@ -38,7 +42,7 @@ openRTSP \
 # conclusion
 cat $logfile | grep Outputting
 echo $line >> $logfile
-printf "Completed execution.\n" | tee -a $logfile
+printf "  Completed execution.\n" | tee -a $logfile
 
 
 # end of script
