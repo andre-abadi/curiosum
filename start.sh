578e5b141c18d075b086d61151b071575754898f
dancingborg <dancingborg@users.noreply.github.com>
2020-08-23 15:31:01 +0800

    change file interval to 1hr (3600s)

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 today=$(date +"%Y-%m-%d-%H-%M-%S")
 printf "This script started at '%s'\n" "${today}"
 mkdir /volume1/transfer/curiosum /volume1/transfer/curiosum/logs /volume1/transfer/curiosum/recordings
-openRTSP -4 -a -P 10 -F /volume1/transfer/curiosum/recordings/${today} rtsp://10.1.1.64:554/s1 > /volume1/transfer/curiosum/logs/${today}.log 2>&1
+openRTSP -4 -a -P 3600 -F /volume1/transfer/curiosum/recordings/${today} rtsp://10.1.1.64:554/s1 > /volume1/transfer/curiosum/logs/${today}.log 2>&1
