477c5e94471bf3be06326c125db5d24564c8de7c
dancingborg <dancingborg@users.noreply.github.com>
2020-08-23 21:16:58 +0800

    log all of the things with tee

--- curiosum.sh
+++ curiosum.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 today=$(date +"%Y-%m-%d-%H-%M-%S-%Z")
-data="/volume1/transfer/curiosum"
 address="rtsp://10.1.2.63:554/s1"
+data="/volume1/transfer/curiosum"
+logfile=$data/logs/$today.log
 duration=10
+line="======================================================"
 let split=duration+1
 
 # user-visible logs
-printf "Curiosum audio-only RTSP recorder.\n"
-printf "  This script started at: '%s'\n" "${today}"
-printf "  Storing data in: '%s'\n" "${data}"
-printf "  Checking that subdirectories /recordings and /logs exist.\n"
-mkdir -p "${data}" "${data}/logs" "${data}/recordings"
-printf "  Intervals will be split every: '%s's\n" "${duration}"
-printf "  Running OpenRTSP command:"
+echo $line >> $logfile
+printf "Curiosum audio-only RTSP recorder.\n" | tee -a $logfile
+printf "  This script started at: '%s'\n" "${today}" | tee -a $logfile
+printf "  Storing data in: '%s'\n" "${data}" | tee -a $logfile
+printf "  Checking that subdirectories /recordings and /logs exist.\n" | tee -a $logfile
+mkdir -p "${data}" "${data}/logs" "${data}/recordings" | tee -a $logfile
+printf "  Logging to: '%s'\n" "${logfile}" | tee -a $logfile
+printf "Running openRTSP for: '%s's\n" "${duration}" | tee -a $logfile
+echo $line >> $logfile
 openRTSP \
     -4 \
     -a \
@@ -21,5 +25,7 @@ openRTSP \
     -P $split \
     -F ${data}/recordings/${today} \
     $address \
-    > ${data}/logs/${today}.log 2>&1
-printf "\nCompleted execution.\n"
+    >> ${data}/logs/${today}.log 2>&1
+cat $logfile | grep Outputting
+echo $line >> $logfile
+printf "Completed execution.\n" | tee -a $logfile
