bd58c58fb1af889081349ae4662f3d4871188cba
dancingborg <dancingborg@users.noreply.github.com>
2020-08-23 21:22:51 +0800

    add -Q and -K options to openRTSP command

--- curiosum.sh
+++ curiosum.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-today=$(date +"%Y-%m-%d-%H-%M-%S-%Z")
+# change these variables as required
 address="rtsp://10.1.2.63:554/s1"
 data="/volume1/transfer/curiosum"
+
+# you shoudln't need to change these variables
+today=$(date +"%Y-%m-%d-%H-%M-%S-%Z")
 logfile=$data/logs/$today.log
 duration=10
 line="======================================================"
+# below  is because otherwise a 1s file gets made after duration
 let split=duration+1
 
-# user-visible logs
+# preamble
 echo $line >> $logfile
 printf "Curiosum audio-only RTSP recorder.\n" | tee -a $logfile
 printf "  This script started at: '%s'\n" "${today}" | tee -a $logfile
@@ -18,14 +22,20 @@ mkdir -p "${data}" "${data}/logs" "${data}/recordings" | tee -a $logfile
 printf "  Logging to: '%s'\n" "${logfile}" | tee -a $logfile
 printf "Running openRTSP for: '%s's\n" "${duration}" | tee -a $logfile
 echo $line >> $logfile
+
+# execution
 openRTSP \
     -4 \
     -a \
+    -K \
+    -Q \
     -d $duration \
     -P $split \
     -F ${data}/recordings/${today} \
     $address \
     >> ${data}/logs/${today}.log 2>&1
+
+# conclusion
 cat $logfile | grep Outputting
 echo $line >> $logfile
 printf "Completed execution.\n" | tee -a $logfile
