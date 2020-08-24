150c28fa1e507e3225a6c416bde5e80ef0471754
dancingborg <dancingborg@users.noreply.github.com>
2020-08-24 08:46:14 +0800

    add daily folder to script

--- curiosum.sh
+++ curiosum.sh
@@ -8,7 +8,8 @@ data="/volume1/transfer/curiosum"
 
 # you shoudln't need to change these variables
 today=$(date +"%Y-%m-%d-%H-%M-%S-%Z")
-logfile=$data/logs/$today.log
+datefolder=$(date +"%Y-%m-%d")
+logfile=$data/logs/$datefolder/$today.log
 duration=3600
 line="======================================================"
 # below  is because otherwise a 1s file gets made after duration
@@ -19,9 +20,10 @@ let split=duration+1
 echo $line >> $logfile
 printf "Curiosum audio-only RTSP recorder.\n" | tee -a $logfile
 printf "  This script started at: '%s'\n" "${today}" | tee -a $logfile
+touch $logfile
 printf "  Storing data in: '%s'\n" "${data}" | tee -a $logfile
 printf "  Checking that subdirectories /recordings and /logs exist.\n" | tee -a $logfile
-mkdir -p "${data}" "${data}/logs" "${data}/recordings" | tee -a $logfile
+mkdir -p "${data}" "${data}/logs/${datefolder}" "${data}/recordings/${datefolder}" | tee -a $logfile
 printf "  Logging to: '%s'\n" "${logfile}" | tee -a $logfile
 printf "  Running openRTSP for: '%s's\n" "${duration}" | tee -a $logfile
 echo $line >> $logfile
@@ -35,9 +37,9 @@ openRTSP \
     -Q \
     -d $duration \
     -P $split \
-    -F ${data}/recordings/${today} \
+    -F ${data}/recordings/${folder}/${today} \
     $address \
-    >> ${data}/logs/${today}.log 2>&1
+    >> $logfile 2>&1
 
 # conclusion
 cat $logfile | grep Outputting
